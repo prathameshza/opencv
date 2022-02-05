d7182db5d42991f78478200ffd371bae6dd6b8cc
prathmesh9090 <46810093+prathmesh9090@users.noreply.github.com>
2022-02-05 17:33:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
 from flask import Flask, render_template, Response
 import cv2
+import http.server
+import socketserver
+
+
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes(pageData, "utf8"))
+        elif self.path.startswith('/image'):
+            self.send_response(200)
+            self.send_header("Content-type", "image/jpeg")
+            self.end_headers()
+
+            ret, frame = cap.read()
+            _, jpg = cv2.imencode(".jpg", frame)
+
+            self.wfile.write(jpg)
+        else:
+            self.send_response(404)
+
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print("Serving at port ", PORT)
+    try:
+        httpd.serve_forever()
+    except:
+        pass
+
 
 app = Flask(__name__)
 
-camera_backends = cv2.videoio_registry.getCameraBackends()
-print(camera_backends)
-print([
-    cv2.videoio_registry.getBackendName(apipref)
-    for apipref in camera_backends
-])
-
-camera = cv2.VideoCapture("/dev/video0", cv2.CAP_V4L2)  # use 0 for web camera
+camera = cv2.VideoCapture("/dev/video0")  # use 0 for web camera
 #  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
 # for local webcam use cv2.VideoCapture(0)
 
